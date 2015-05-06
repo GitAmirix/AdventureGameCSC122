de3301c102639fe73142490069158a2deb2c96d0
dsa030 <dsa030@latech.edu>
2015-05-06 10:31:36 -0500

    That message

--- Adventure Game/src/game/Layout.java
+++ Adventure Game/src/game/Layout.java
@@ -10,7 +10,9 @@ import javax.imageio.*;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Map;
-
+/*This is a work of fiction. Names, characters, places and incidents either are
+products of the author’s imagination or are used fictitiously. Any resemblance to actual events
+or locales or persons, living or dead, is entirely coincidental.*/
 public class Layout {
 	private static final String GAME_TITLE = "Murder at Gourd Mansion";
 
